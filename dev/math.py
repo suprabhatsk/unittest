ea2c52d4b70ad8d8da631bbb3bcb3eeca37a8e5c
Suprabhat Kumar <suprabhat.sk@gmail.com>
2022-08-14 19:38:31 +0100

    Commit test

--- dev/math.py
+++ dev/math.py
@@ -1,52 +1,26 @@
-def add_num(a, b):
-    return (a + b)
 
-def sub_num(a, b):
-    return (a - b)
+class Maths:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Maths:
-
-#     def __init__(self):
-#         pass
+    def __init__(self):
+        pass
     
-#     def add_num(self, a, b):
-#         return (a + b)
+    def add_num(self, a, b):
+        return (a + b)
     
-#     def divide(self, a, b):
-#         if a == 0:
-#             raise ZeroDivisionError("denominator value is zero can not divide")
-#         else:
-#             return int(b/a)
+    def divide(self, a, b):
+        if a == 0:
+            raise ZeroDivisionError("denominator value is zero can not divide")
+        else:
+            return int(b/a)
         
-#     def data(self, a):
-#         if a < 1:
-#             raise ValueError("less the 1 Error")
+    def data(self, a):
+        if a < 1:
+            raise ValueError("less the 1 Error")
     
-        
+      
 
-# if __name__ == "__main__":
-#     cl_ = Maths()
-#     print(cl_.add_num(1, 2))
-#     print(cl_.divide(2, 4))
-#     print(cl_.data(0))
+if __name__ == "__main__":
+    cl_ = Maths()
+    print(cl_.add_num(1, 2))
+    print(cl_.divide(2, 4))
+    print(cl_.data(0))
